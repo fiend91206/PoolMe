50bfebc05e89e7bb30b1dccac28c49e8aaf36b46
Paul Beresuita <sbfifa10@gmail.com>
2012-04-05 22:18:54 -0400

    -Added ability to send messages to another device -Added a "notification" when a message sent was successful

--- src/pool/me/Messages.java
+++ src/pool/me/Messages.java
@@ -4,4 +4,5 @@ import android.app.Activity;
 
 public class Messages extends Activity{
 
+		
 }
--- src/pool/me/New_Message.java
+++ src/pool/me/New_Message.java
@@ -1,7 +1,52 @@
 package pool.me;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class New_Message extends Activity{
 
+	AutoCompleteTextView messageNumber;
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_message);
+	
+        messageNumber = (AutoCompleteTextView)findViewById(R.id.to);
+	}
+	
+	public void sendMessage(View v)
+	{
+		String _messageNumber=messageNumber.getText().toString();
+		String messageText = "Hi , Just SMSed to say hello";
+		String sent = "SMS_SENT";
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+            new Intent(sent), 0);
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+            	if(getResultCode() == Activity.RESULT_OK)
+                {
+                  Toast.makeText(getBaseContext(), "SMS sent",
+                                Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                	Toast.makeText(getBaseContext(), "SMS could not sent",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new IntentFilter(sent));
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(_messageNumber, null, messageText, sentPI, null);
+	}
 }
