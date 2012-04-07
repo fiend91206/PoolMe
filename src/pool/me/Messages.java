aaa4f7af361e5aa983d8bde37c328ff87e4f1b0e
Paul Beresuita <sbfifa10@gmail.com>
2012-04-07 17:19:26 -0400

    -Added Transition to the New Message view

--- src/pool/me/Messages.java
+++ src/pool/me/Messages.java
@@ -1,8 +1,28 @@
 package pool.me;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
-public class Messages extends Activity{
+public class Messages extends Activity implements OnClickListener{
 
-		
+	private View newMessageButton;
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.messages);
+	
+       this.newMessageButton =  (ImageButton)findViewById(R.id.newMessageButton);
+        
+       newMessageButton.setOnClickListener((OnClickListener) this); 
+	}
+	
+	public void onClick(View view){
+	
+		this.startActivity(new Intent(this, New_Message.class));
+	}
+
 }
