48bd6a8d2b3c5675026d00a7495e4de8a7c008ec
Pujita Vijayvargiya <pujita90@gmail.com>
2012-04-09 05:49:28 -0400

    screen capture

--- src/pool/me/Login.java
+++ src/pool/me/Login.java
@@ -60,6 +60,11 @@ public class Login extends Activity implements OnClickListener{
 			{
 				User u = new User();
 				u = db.getUser(email);
+				u.setFirstName("Shashank");
+				u.setLastName("Chamoli");
+				u.setContactNumber(77012567);
+				u.setAboutMe("Hello. I'm new here.");
+				u.setSourceLocation("900 Hemphil Avenue");
 				Session.getInstance().setUser(u);
 				startActivity(new Intent(this, Main.class));
 				finish();
